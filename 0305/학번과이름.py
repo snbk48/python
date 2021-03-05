0077d41db7b7e90d5b8e76f06907c25b403048cf
snbk48 <80086318+snbk48@users.noreply.github.com>
2021-03-05 15:58:10 +0900

    Add files via upload

---
+++ 0305/학번과이름.py
@@ -0,0 +1,6 @@
+#학번을 입력받아 stdnum이라는 변수에 저장합니다.
+stdnum = input("학번 : ")
+#이름을 입력받아 stdnum이라는 변수에 저장합니다.
+stdname = input("이름 : ")
+#학번과 이름을 출력합니다.
+print("당신의 학번은 ", stdnum, "이고 이름은 ", stdname, "입니다.")
