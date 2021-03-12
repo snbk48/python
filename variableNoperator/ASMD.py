ce1b71d9cb5e48a799d56251fde1cec90d1222a8
snbk48 <80086318+snbk48@users.noreply.github.com>
2021-03-12 16:08:20 +0900

    Add files via upload

---
+++ variableNoperator/ASMD.py
@@ -0,0 +1,9 @@
+#두정수를 받아 사칙연산 결과를 출력
+a = int(input("첫번째 숫자 : "))
+b = int(input("두번째 숫자 : "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+
+
---
+++ variableNoperator/strNint.py
@@ -0,0 +1,11 @@
+a = input("첫 번째 숫자 : ")
+b = input("두 번째 숫자 : ")
+print(a+b)
+print("hello" + "python")
+
+#문자열을 숫자로 변환시키는 함수int
+a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+print(a+b)
+
+
